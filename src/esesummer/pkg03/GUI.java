7f802eb6829551ae041ee638f0b62b707c7e0f92
Nicolò Vescera <slayercandy@icloud.com>
2016-08-25 16:35:22 +0200

    Ripristinato salvataggio perso. Continuo a creare

--- src/esesummer/pkg03/GUI.java
+++ src/esesummer/pkg03/GUI.java
@@ -1,24 +1,27 @@
 package esesummer.pkg03;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+
 
 public class GUI extends JFrame{
     JPanel panel = new JPanel();
     JTextField textbox = new JTextField(50);
+    JTextArea testo = new JTextArea("Il cane mangia il gatto come il topo mangia il cane");
     String array = "Il cane mangia il gatto come il topo mangia il cane";
     
     public GUI(){
         textbox.getDocument().addDocumentListener(new MyDocumentListener());
         
-        panel.add(textbox);
+        /*panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
         
-        this.add(panel,"Center");
+        panel.add(textbox);
+        panel.add(testo);
+        */
+        this.add(textbox,"North");
+        this.add(testo,"Center");
         
         this.pack();
         this.setVisible(true);
--- src/esesummer/pkg03/MyDocumentListener.java
+++ src/esesummer/pkg03/MyDocumentListener.java
@@ -1,5 +1,6 @@
 package esesummer.pkg03;
 
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.event.DocumentEvent;
@@ -9,24 +10,41 @@ import javax.swing.text.BadLocationException;
 public class MyDocumentListener implements DocumentListener{
 
     @Override
-            public void insertUpdate(DocumentEvent e) {
+    public void insertUpdate(DocumentEvent e) {
         try {
-            System.out.println(e.getDocument().getText(0, e.getDocument().getLength()));
+            System.out.println(getTextFieldText(e));
         } catch (BadLocationException ex) {
             Logger.getLogger(MyDocumentListener.class.getName()).log(Level.SEVERE, null, ex);
         }
-            }
+    }
 
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-               // System.out.println(textbox.getText());
-            }
+    @Override
+        public void removeUpdate(DocumentEvent e) {
+            // System.out.println(textbox.getText());
+        }
 
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String getTextFieldText(DocumentEvent e) throws BadLocationException{
+        return e.getDocument().getText(0, e.getDocument().getLength());
+    }
+    
+    private String[] stringToArray(String text){
+        ArrayList<String> array = new ArrayList();
+        for(int i = 0; i < text.length(); i++){
+            array.add(text.substring(0,text.indexOf(' ')));
+            text = text.substring(text.indexOf(' ')+1);
+        }
+        array.add(text);
         
+        String[] end = new String[array.size()];
+        array.toArray(end);
+        
+        return end;
+    }
         
 
 }
